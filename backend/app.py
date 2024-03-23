7661ab29d7701ba5e02914db9585dba77f780c1c
Meer Singh Bhai <meersinghbhai@gmail.com>
2024-03-23 16:04:53 -0400

    Implemented json search and fixed relevant bugs

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
 import jsonpickle
 import numpy as np
+import sys
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -29,7 +30,14 @@ with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api
 #retrieve set of artists in the database
 big_songs_set = jsonpickle.decode(big_songs_json_obj)
 
-    
+# with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_songs_set.json", "r") as openfile: 
+#   small_songs_json_obj = json.load(openfile)
+#retrieve set of artists in the database
+small_songs_list = small_songs_df.index.tolist()
+
+debug = True
+
+np.set_printoptions(threshold=sys.maxsize)
 
 app = Flask(__name__)
 CORS(app)
@@ -39,20 +47,32 @@ def json_search(query):
     if query in big_songs_set:
         song_ft = big_songs_df.loc[query]
         song_vect = song_ft.to_numpy()
-        print(np.shape(song_vect))
-        print(np.shape(small_songs_np))
-        similarity_index = np.dot(song_vect, small_songs_np)
+        if song_vect.ndim > 1:
+            song_vect = song_vect[0]
+        print("song vector")
+        print(song_vect)
+        similarity_index = np.dot(song_vect, small_songs_np.T) / (np.linalg.norm(song_vect) * np.linalg.norm(small_songs_np))
+        rankings = np.argsort(similarity_index)
+        print("similarity index")
+        print(np.shape(similarity_index))
         print(similarity_index)
-
-    return small_songs_df.to_json()
+        print("rankings:")
+        print(rankings)
+        res = []
+        for r in rankings[:10]:
+            res.append(small_songs_list[r])
+        print(res)
+        return json.dumps(res)
+    else: 
+        return json.dumps(small_songs_list[:10])
 
 @app.route("/")
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
+@app.route("/songs")
 
-def episodes_search():
+def song_search():
     text = request.args.get("title")
     return json_search(text)
 
