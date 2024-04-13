e100fbc9d0b30ac936a8d8a856b051180c12995b
Meer Singh Bhai <meersinghbhai@gmail.com>
2024-04-13 18:11:41 -0400

    Data scraping

--- backend/spotify_api/spot_main.py
+++ backend/spotify_api/spot_main.py
@@ -8,48 +8,59 @@ import pandas as pd
 import jsonpickle
 from json import JSONEncoder
 from pathlib import Path
-
+import re
+from lyricsgenius import Genius
 load_dotenv()
 
 #GLOBAL VARIABLES
 
 # with open("backend/spotify_api/database_jsons/small_artist_set.json", "r") as openfile: 
 #   artist_json_obj = json.load(openfile)
-#retrieve set of artists in the database
+# #retrieve set of artists in the database
 
 # artist_set = jsonpickle.decode(artist_json_obj)
+
+
 artist_set = set()
 
 
 # with open("backend/spotify_api/database_jsons/small_songs_set.json", "r") as openfile: 
 #   song_json_obj = json.load(openfile)
-#retrieve set of artists in the database
+# #retrieve set of artists in the database
 
 # songs_set = jsonpickle.decode(song_json_obj)
+
+
 songs_set = set()
 
 
 #retrieve dataframes
-# with open("backend/spotify_api/database_jsons/artist_dataframes.json", "r") as openfile: 
-#   dataframes = [pd.read_json("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/artist_dataframes.json", orient = "split")]
+with open("backend/spotify_api/database_jsons/artist_dataframes.json", "r") as openfile: 
+  dataframes = [pd.read_json("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/artist_dataframes.json", orient = "split")]
 
-dataframes = []
+# dataframes = []
 
 #Feature to index for song by feature matrix. Please update this as features 
 #increase. This vector contains numbers only (to be used for similarity analysis)
 feature_dict = {
-  "acousticness" : 0,
-  "danceability" : 1,
-  "energy" : 2,
-  "instrumentalness" : 3,
-  "key" : 4,
-  "liveness" : 5,
-  "loudness" : 6,
-  "mode" : 7,
-  "speechiness" : 8,
-  "tempo" : 9,
-  "time_signature" : 10,
-  "valence" : 11 
+  "artist" : 1,
+  "explicit" : 2,
+  "id" : 3,
+  "popularity" : 4,
+  "title" : 5,
+  "energy" : 6,
+  "liveness" : 7,
+  "tempo" : 8,
+  "speechiness" : 9,
+  "acousticness" : 10,
+  "instrumentalness" : 11,
+  "time_signature" : 12,
+  "danceability" : 13,
+  "key" : 14,
+  "duration_ms" : 15, 
+  "loudness" : 16,
+  "valence" : 17,
+  "mode" : 18
 }
 
 collumns = feature_dict.keys()
@@ -169,6 +180,21 @@ def update_artist_songs(res):
     else:
       songs_set.add(i)
   return res
+
+def get_lyrics(token, song):
+
+  url_1 = "https://api.spotify.com/v1/search"
+  headers = get_header(token)
+  query = f"?q={song}&type=track&limit=1"
+  query_url = url_1 + query 
+  result_1 = get(query_url, headers=headers)
+  json_result_1 = json.loads(result_1.content)['tracks']['items'][0]['id']
+  print(json_result_1)
+  song_id = str(json_result_1)
+  url = f"https://spclient.wg.spotify.com/color-lyrics/v2/track/{song_id}"
+  result_2 = get(url, headers=headers)
+  # json_result_2 = json.loads(result.content)
+  print(result_2)
   
 
 
@@ -177,7 +203,32 @@ def update_artist_songs(res):
 # Main Function here onwards:
   
 curr_token = get_token()
-# token = "current token"
+
+artist_list = ['drake']
+
+print(dataframes)
+
+# Genius Lyrics code
+token = "piO53A6NuGW0cKJ_YPDC4UpXjO5Yc-dJaBUwSt3yzdQZGiA4TxfxAU75nQz2D_p9"
+genius_token = Genius(token)
+for artist_name in artist_list:
+  artist = genius_token.search_artist(artist_name)
+  for song in artist.songs:
+    print(song.lyrics)
+  # artist.save_lyrics()
+    
+# while True:
+#     try:
+#         artist.append(genius.search_artist(artist, max_songs=10000))
+#         break
+#     except:
+#         pass
+
+
+
+
+# print(f"Current token is: {curr_token}")
+# token = "BQD4VVXcg2x59WuetMCD4obIVx0I1oRBPpNzBJeVCPydM68kDlnMziFpOasAsEp4UIgKEDnaZjq-r9sNYLGgfN9eMsXSO_EhUjV5s8TauoWFscgsDug"
 #add artists in the list below
 # artist_list = ['21 Savage',
     # 'Adele',
@@ -278,201 +329,208 @@ curr_token = get_token()
     # 'Zach Bryan',
     # 'floyymenor',
     # '¥$' ]
-artist_list = [ "Anna B Savage",
-    "Automatic",
-    "Bambara",
-    "Barrie",
-    "Bbymutha",
-    "Bendik Giske",
-    "Billy Woods",
-    "Black Country, New Road",
-    "Brutus",
-    "Cassandra Jenkins",
-    "Chastity Belt",
-    "Chloe Moriondo",
-    "Chris Cohen",
-    "Christian Lee Hutson",
-    "Circuit des Yeux",
-    "Claud",
-    "Cleo Sol",
-    "Clipping.",
-    "Crack Cloud",
-    "Dehd",
-    "Disq",
-    "Dorian Electra",
-    "Dry Cleaning",
-    "Duval Timothy",
-    "Ela Minus",
-    "Empath",
-    "Fenne Lily",
-    "Fieh",
-    "Field Medic",
-    "Gabriels",
-    "Ganser",
-    "Girlpool",
-    "Gleemer",
-    "Goat Girl",
-    "Hachiku",
-    "Hand Habits",
-    "Hannah Diamond",
-    "Hatchie",
-    "Hater",
-    "Helena Deland",
-    "Hilary Woods",
-    "Horse Jumper of Love",
-    "Ian Sweet",
-    "Illuminati Hotties",
-    "Jaye Jayle",
-    "Jeff Rosenstock",
-    "Jesca Hoop",
-    "Jess Williamson",
-    "Jockstrap",
-    "Kaina",
-    "Katie Dey",
-    "Katy Kirby",
-    "Keeley Forsyth",
-    "Kelly Moran",
-    "Kero Kero Bonito",
-    "Khruangbin",
-    "Kikagaku Moyo",
-    "Klein",
-    "Lala Lala",
-    "Land of Talk",
-    "Lingua Ignota",
-    "Lisa/Liza",
-    "Little Simz",
-    "Lomelda",
-    "Lonnie Holley",
-    "Lucy Dacus",
-    "Mabe Fratti",
-    "Madeline Kenney",
-    "Mapache",
-    "Maria Somerville",
-    "Marie Davidson",
-    "Marlowe",
-    "Mary Lattimore",
-    "Maston",
-    "Matana Roberts",
-    "Melkbelly",
-    "Mereba",
-    "Methyl Ethel",
-    "Mia Gargaret",
-    "Michael Nau",
-    "Molly Burch",
-    "Molly Sarlé",
-    "Moor Jewelry",
-    "MorMor",
-    "Nadia Reid",
-    "Natalie Prass",
-    "Nation of Language",
-    "Negative Gemini",
-    "Nick Hakim",
-    "Nicolas Jaar",
-    "Nite Jewel",
-    "No Joy",
-    "Norman Westberg",
-    "Noveller",
-    "Okay Kaya",
-    "Omni",
-    "Oso Oso",
-    "Overcoats",
-    "Palm",
-    "Pan Amsterdam",
-    "Pendant",
-    "Perfume Genius",
-    "Pile",
-    "Pinegrove",
-    "Porridge Radio",
-    "Posse",
-    "Protomartyr",
-    "Puma Blue",
-    "Pure X",
-    "Quelle Chris",
-    "R.A.P. Ferreira",
-    "Ratboys",
-    "Remember Sports",
-    "Renata Zeiguer",
-    "Ric Wilson",
-    "Richard Dawson",
-    "Rosie Tucker",
-    "Sammus",
-    "Sarah Mary Chadwick",
-    "Sassy 009",
-    "Shamir",
-    "Shannon Lay",
-    "Sheer Mag",
-    "Shygirl",
-    "Sleaford Mods",
-    "Snapped Ankles",
-    "Soccer Mommy",
-    "Soccer96",
-    "Solange",
-    "Spellling",
-    "Squid",
-    "Stella Donnelly",
-    "Steve Lacy",
-    "Sudan Archives",
-    "Sufjan Stevens",
-    "Summer Walker",
-    "Sunflower Bean",
-    "Surf Curse",
-    "Sylvan Esso",
-    "Tasha",
-    "Tennis",
-    "The Beths",
-    "The Comet Is Coming",
-    "The Garden",
-    "The Japanese House",
-    "The Weather Station",
-    "Thou",
-    "Tirzah",
-    "Tomberlin",
-    "TOPS",
-    "Toro y Moi",
-    "Trace Mountains",
-    "Vagabon",
-    "Vessel",
-    "Waxahatchee",
-    "Weatherday",
-    "Wednesday",
-    "Weyes Blood",
-    "White Reaper",
-    "Wiki",
-    "William Tyler",
-    "Wye Oak",
-    "Xenia Rubinos",
-    "Yaeji",
-    "Yves Tumor",
-    "Zola Jesus"]
-artist_to_song_dict , song_to_artist_dict, song_id_to_name = create_top_songs_matrix_by(curr_token, artist_list)
+# artist_list = [ "Anna B Savage",
+#     "Automatic",
+#     "Bambara",
+#     "Barrie",
+#     "Bbymutha",
+#     "Bendik Giske",
+#     "Billy Woods",
+#     "Black Country, New Road",
+#     "Brutus",
+#     "Cassandra Jenkins",
+#     "Chastity Belt",
+#     "Chloe Moriondo",
+#     "Chris Cohen",
+#     "Christian Lee Hutson",
+#     "Circuit des Yeux",
+#     "Claud",
+#     "Cleo Sol",
+#     "Clipping.",
+#     "Crack Cloud",
+#     "Dehd",
+#     "Disq",
+#     "Dorian Electra",
+#     "Dry Cleaning",
+#     "Duval Timothy",
+#     "Ela Minus",
+#     "Empath",
+#     "Fenne Lily",
+#     "Fieh",
+#     "Field Medic",
+#     "Gabriels",
+#     "Ganser",
+#     "Girlpool",
+#     "Gleemer",
+#     "Goat Girl",
+#     "Hachiku",
+#     "Hand Habits",
+#     "Hannah Diamond",
+#     "Hatchie",
+#     "Hater",
+#     "Helena Deland",
+#     "Hilary Woods",
+#     "Horse Jumper of Love",
+#     "Ian Sweet",
+#     "Illuminati Hotties",
+#     "Jaye Jayle",
+#     "Jeff Rosenstock",
+#     "Jesca Hoop",
+#     "Jess Williamson",
+#     "Jockstrap",
+#     "Kaina",
+#     "Katie Dey",
+#     "Katy Kirby",
+#     "Keeley Forsyth",
+#     "Kelly Moran",
+#     "Kero Kero Bonito",
+#     "Khruangbin",
+#     "Kikagaku Moyo",
+#     "Klein",
+#     "Lala Lala",
+#     "Land of Talk",
+#     "Lingua Ignota",
+#     "Lisa/Liza",
+#     "Little Simz",
+#     "Lomelda",
+#     "Lonnie Holley",
+#     "Lucy Dacus",
+#     "Mabe Fratti",
+#     "Madeline Kenney",
+#     "Mapache",
+#     "Maria Somerville",
+#     "Marie Davidson",
+#     "Marlowe",
+#     "Mary Lattimore",
+#     "Maston",
+#     "Matana Roberts",
+#     "Melkbelly",
+#     "Mereba",
+#     "Methyl Ethel",
+#     "Mia Gargaret",
+#     "Michael Nau",
+#     "Molly Burch",
+#     "Molly Sarlé",
+#     "Moor Jewelry",
+#     "MorMor",
+#     "Nadia Reid",
+#     "Natalie Prass",
+#     "Nation of Language",
+#     "Negative Gemini",
+#     "Nick Hakim",
+#     "Nicolas Jaar",
+#     "Nite Jewel",
+#     "No Joy",
+#     "Norman Westberg",
+#     "Noveller",
+#     "Okay Kaya",
+#     "Omni",
+#     "Oso Oso",
+#     "Overcoats",
+#     "Palm",
+#     "Pan Amsterdam",
+#     "Pendant",
+#     "Perfume Genius",
+#     "Pile",
+#     "Pinegrove",
+#     "Porridge Radio",
+#     "Posse",
+#     "Protomartyr",
+#     "Puma Blue",
+#     "Pure X",
+#     "Quelle Chris",
+#     "R.A.P. Ferreira",
+#     "Ratboys",
+#     "Remember Sports",
+#     "Renata Zeiguer",
+#     "Ric Wilson",
+#     "Richard Dawson",
+#     "Rosie Tucker",
+#     "Sammus",
+#     "Sarah Mary Chadwick",
+#     "Sassy 009",
+#     "Shamir",
+#     "Shannon Lay",
+#     "Sheer Mag",
+#     "Shygirl",
+#     "Sleaford Mods",
+#     "Snapped Ankles",
+#     "Soccer Mommy",
+#     "Soccer96",
+#     "Solange",
+#     "Spellling",
+#     "Squid",
+#     "Stella Donnelly",
+#     "Steve Lacy",
+#     "Sudan Archives",
+#     "Sufjan Stevens",
+#     "Summer Walker",
+#     "Sunflower Bean",
+#     "Surf Curse",
+#     "Sylvan Esso",
+#     "Tasha",
+#     "Tennis",
+#     "The Beths",
+#     "The Comet Is Coming",
+#     "The Garden",
+#     "The Japanese House",
+#     "The Weather Station",
+#     "Thou",
+#     "Tirzah",
+#     "Tomberlin",
+#     "TOPS",
+#     "Toro y Moi",
+#     "Trace Mountains",
+#     "Vagabon",
+#     "Vessel",
+#     "Waxahatchee",
+#     "Weatherday",
+#     "Wednesday",
+#     "Weyes Blood",
+#     "White Reaper",
+#     "Wiki",
+#     "William Tyler",
+#     "Wye Oak",
+#     "Xenia Rubinos",
+#     "Yaeji",
+#     "Yves Tumor",
+#     "Zola Jesus"]
 
-for a in artist_list:
-  #returns dataframe of artist a's song by feature matrix
-  res = make_artist_track_matrix(artist_to_song_dict, song_to_artist_dict,song_id_to_name, a)
-  update_artist_songs(res)
-  artist_set.add(a)
-  dataframes.append(res)
-  # print(res)
+# artist_to_song_dict , song_to_artist_dict, song_id_to_name = create_top_songs_matrix_by(curr_token, artist_list)
+
+# for a in artist_list:
+#   #returns dataframe of artist a's song by feature matrix
+#   res = make_artist_track_matrix(artist_to_song_dict, song_to_artist_dict,song_id_to_name, a)
+#   update_artist_songs(res)
+#   artist_set.add(a)
+#   dataframes.append(res)
+#   print(res)
+
+# song_list = ["Happier"]
+# for s in song_list:
+#   get_lyrics(curr_token, s)
+
+###############################################################################
+
+#SAVE COLLECTED DATA: BE CAREFUL NOT TO OVERRIDE FILES
 
 #return set of artists in the database
-new_artist_json_obj = jsonpickle.encode(artist_set)
-with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_artist_list.json", "w") as openfile:
-  json.dump(new_artist_json_obj, openfile)
+# new_artist_json_obj = jsonpickle.encode(artist_set)
+# with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_artist_list.json", "w") as openfile:
+#   json.dump(new_artist_json_obj, openfile)
 
-#return songs set to database
-new_songs_json_obj = jsonpickle.encode(songs_set)
-with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_songs_set.json", "w") as openfile:
-  json.dump(new_songs_json_obj, openfile)
+# #return songs set to database
+# new_songs_json_obj = jsonpickle.encode(songs_set)
+# with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_songs_set.json", "w") as openfile:
+#   json.dump(new_songs_json_obj, openfile)
 
-#return list of dataframes
-p = Path("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_artist_dataframes.json")
-fin_dataframe = pd.concat(dataframes, join='outer', axis=0)
-print(fin_dataframe)
-fin_dataframe.to_json(path_or_buf=p, orient='split')
+# #return list of dataframes
+# p = Path("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_artist_dataframes.json")
+# fin_dataframe = pd.concat(dataframes, join='outer', axis=0)
+# print(fin_dataframe)
+# fin_dataframe.to_json(path_or_buf=p, orient='split')
 
-
-
-
+###############################################################################
 
 
 
