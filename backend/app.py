3d8e0099aa427b29648a75b87bf65ea3834bacb6
Meer Singh Bhai <meersinghbhai@gmail.com>
2024-04-16 01:25:24 -0400

    updates

--- backend/app.py
+++ backend/app.py
@@ -22,21 +22,21 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # Specify the path to the JSON file relative to the current script
 
 # json_file_path = "../backend/spotify_api/database_jsons/big_artist_dataframes.json"
-json_file_path2 = "../backend/spotify_api/database_jsons/small_artist_dataframes.json"
+# json_file_path2 = "../backend/spotify_api/database_jsons/small_artist_dataframes.json"
 
 # # Assuming your JSON data is stored in a file named 'init.json'
 # big_songs_df = pd.read_json(json_file_path, orient = "split")
 
-small_songs_df = pd.read_json(json_file_path2, orient="split")
+# small_songs_df = pd.read_json(json_file_path2, orient="split")
 # small_songs_np = small_songs_df.to_numpy()
 
 # with open("../backend/spotify_api/database_jsons/big_songs_list.json", "r") as openfile: 
 #   big_songs_set = json.load(openfile)
 
-with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_songs_set.json", "r") as openfile: 
-  small_songs_json_obj = json.load(openfile)
-#retrieve set of artists in the database
-small_songs_list = small_songs_df.index.tolist()
+# with open("/Users/meer/Desktop/4300/4300-Flask-Template-JSON/backend/spotify_api/database_jsons/small_songs_set.json", "r") as openfile: 
+#   small_songs_json_obj = json.load(openfile)
+# #retrieve set of artists in the database
+# small_songs_list = small_songs_df.index.tolist()
 
 artist_df = pd.read_csv('../backend/kaggle_data/new_data.csv')
 
